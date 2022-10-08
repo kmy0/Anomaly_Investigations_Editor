f511135186724c2af45333917816a4567bd3a0b2
kmy0 <93156259+kmy0@users.noreply.github.com>
2022-10-08 23:47:34 +0200

    Update Anomaly Investigations Editor.lua

--- reframework/autorun/Anomaly Investigations Editor.lua
+++ reframework/autorun/Anomaly Investigations Editor.lua
@@ -8,6 +8,25 @@ local spacewatcher = nil
 local create_rand_myst = sdk.find_type_definition('snow.quest.nRandomMysteryQuest'):get_method('CreateRandomMysteryQuest')
 local check_auth = sdk.find_type_definition('snow.quest.nRandomMysteryQuest'):get_method('checkRandomMysteryQuestOrderBan')
 
+local window_flags = 0x10120
+local window_pos = Vector2f.new(400, 200)
+local window_pivot = Vector2f.new(0, 0)
+local window_size = Vector2f.new(560, 820)
+local is_opened = false
+local version = '1.2.1'
+
+local table_flags = 0x12780
+local table_row_flags = 0x1
+local table_ = {
+	{'','1    ','1-4','1-5','2-6','3-6','4-6','5-6'},
+	{'1',100,25,10,0,0,0,0},
+	{'2',0,40,25,10,0,0,0},
+	{'3',0,30,35,25,15,0,0},
+	{'4',0,5,25,40,30,20,0},
+	{'5',0,0,5,20,40,35,35},
+	{'6',0,0,0,5,15,45,65},
+}
+
 local monster_main_array = {}
 local monster_extra_array = {}
 local monster_intruder_array = {}
@@ -40,7 +59,16 @@ local auth_status_dict = {
 					[3]='Monster Rank Too High For Quest Level',
 					[4]='Duplicate Monsters'
 					}
-
+local rand_rank = {
+			['1']=0,
+			['1-4']=107,
+			['1-5']=19,
+			['2-6']=1,
+			['3-6']=349,
+			['4-6']=351,
+			['5-6']=350
+}
+local rand_rank_array = {}
 local change_vals = true
 local filter = ''
 local filter_c = true
@@ -75,6 +103,7 @@ local time_limit = 50
 local hunter_num = 4
 local tod = 1
 local target_num = 1
+local rand_id = 0
 
 
 for id,data in pairs(monsters) do
@@ -87,6 +116,10 @@ for name,id in pairs(maps) do
 end
 table.sort(maps_array)
 
+for k,_ in pairs(rand_rank) do
+	table.insert(rand_rank_array,k)
+end
+table.sort(rand_rank_array)
 
 local function get_questman()
     if not questman then
@@ -123,7 +156,7 @@ local function get_mystery_quest_data_table()
         local quest_no = quest:get_field('_QuestNo')
         if quest_no ~= -1 then
             local quest_monsters = quest:get_field('_BossEmType')
-            local monster0 = quest_monsters:get_element(0):get_field('value__')
+            local monster0 = quest_monsters:call('get_Item',0)
             local monster0_name = monsters[ tostring(monster0) ]['name']
             local quest_lvl = quest:get_field('_QuestLv')
             local map_no = quest:get_field('_MapNo')
@@ -142,9 +175,9 @@ local function get_mystery_quest_data_table()
                                     _BaseTime=quest:get_field('_BaseTime'),
                                     _HuntTargetNum=quest:get_field('_HuntTargetNum'),
                                     monster0=monster0,
-                                    monster1=quest_monsters:get_element(1):get_field('value__'),
-                                    monster2=quest_monsters:get_element(2):get_field('value__'),
-                                    monster5=quest_monsters:get_element(5):get_field('value__'),
+                                    monster1=quest_monsters:call('get_Item',1),
+                                    monster2=quest_monsters:call('get_Item',2),
+                                    monster5=quest_monsters:call('get_Item',5),
                                     _TimeLimit=quest:get_field('_TimeLimit'),
                                     _QuestLife=quest:get_field('_QuestLife'),
                                     _StartTime=quest:get_field('_StartTime'),
@@ -173,7 +206,7 @@ local function reset_data(skip)
 	auth_check = true
 end
 
-local function generate_random()
+local function generate_random(id)
 	if quest_count == 120 then return end
 
 	local mystery_data = sdk.create_instance('snow.quest.RandomMysteryQuestData')
@@ -186,9 +219,7 @@ local function generate_random()
 
 	mystery_data:set_field('_QuestLv',200)
 	em_types = mystery_data:get_field('_BossEmType')
-	em_types:call('set_Item',0,1)
-	em_types:call('set_Item',1,1)
-	em_types:call('set_Item',2,1)
+	em_types:call('set_Item',0,id)
 
 	create_rand_myst:call(get_questman(),mystery_data,1,mystery_index,mystery_quest_no,true)
 
@@ -299,6 +330,7 @@ local function edit_quest(mystery_data)
 	seed:set_field('_QuestLife',quest_life)
 	seed:set_field('_QuestOrderNum',hunter_num)
 	seed:set_field('_StartTime',time_of_day[ time_of_day_array[ tod ] ])
+	seed:set_field('_MysteryLv',200)
 	seed:call('setEnemyTypes',em_types)
 	mystery_seeds:call('set_Item',seed_index,seed)
 
@@ -406,6 +438,26 @@ local function quest_check(mystery_data)
 	auth_check = false
 end
 
+local function wipe()
+	local mystery_quest_data = get_questman():get_field('_RandomMysteryQuestData'):get_elements()
+	local quest_save_data = get_questman():get_field('<SaveData>k__BackingField')
+	local mystery_seeds = quest_save_data:get_field('_RandomMysteryQuestSeed')
+	local quest_no = nil
+	local newest_no = mystery_quests[ mystery_quests_names[1] ]['quest_no']
+
+	for _,quest in ipairs(mystery_quest_data) do
+		quest_no = quest:get_field('_QuestNo')
+		if not quest:get_field('_IsLock') and quest_no ~= -1 and quest_no ~= newest_no then
+			seed = get_questman():call('getRandomQuestSeedFromQuestNo',quest:get_field('_QuestNo'))
+			seed_index = mystery_seeds:call('IndexOf',seed)
+			quest:call('clear')
+			seed:call('clear')
+			mystery_seeds:call('set_Item',seed_index,seed)
+		end
+	end
+	reset_data(true)
+end
+
 
 sdk.hook(check_auth,
 	function(args)
@@ -434,121 +486,167 @@ sdk.hook(sdk.find_type_definition('snow.gui.fsm.GuiFsmBaseManager`1<snow.gui.fsm
 )
 
 
-
 if sdk.get_managed_singleton('snow.gui.fsm.questcounter.GuiQuestCounterFsmManager') then qc_open = true end
 
 
-
-re.on_draw_ui(function()
-	if imgui.tree_node('Anomaly Investigations Editor') then
-
-		if get_spacewatcher() then
-			game_state = get_spacewatcher():get_field('_GameState')
-		end
-
-		if qc_open or game_state ~= 4 or get_questman():call('isActiveQuest') then
-			imgui.text_colored('Mod works only in the lobby with quest counter closed.', 0xff1947ff)
-		else
-
-			if get_questman() and not dumped and game_state == 4
-			or game_state == 4 and prev_game_state ~= 4 then
-				reset_data()
-			end
-
-			if map_c then get_arrays() end
-
-			if filter_c then filter_names() end
-
-			_,force_auth_pass = imgui.checkbox('Force Authorization Pass', force_auth_pass)
-			filter_c,filter = imgui.input_text('Filter',filter)
-			id_c,id_pick = imgui.combo('Quest',id_pick,mystery_quests_names_filtered)
-	        quest_id = mystery_quests_names_filtered[ id_pick ]
-	        quest = mystery_quests[quest_id]
-
-	        if quest then
-		        imgui.text('Quest Level: ')
-		        imgui.same_line()
-		        imgui.text_colored(quest['_QuestLv'], 0xff27f3f5)
-		        imgui.text('Map: ')
-		        imgui.same_line()
-		        imgui.text_colored(maps_table[ quest['_MapNo'] ], 0xff27f3f5)
-		        imgui.text('Monster 1: ')
-				imgui.same_line()
-		        imgui.text_colored(monsters[ tostring(quest['monster0']) ]['name'], 0xff27f3f5)
-		        imgui.text('Monster 2: ')
-		        imgui.same_line()
-		        imgui.text_colored(monsters[ tostring(quest['monster1']) ]['name'], 0xff27f3f5)
-		        imgui.text('Monster 3: ')
-		        imgui.same_line()
-		        imgui.text_colored(monsters[ tostring(quest['monster2']) ]['name'], 0xff27f3f5)
-		        imgui.text('Intruder: ')
-		        imgui.same_line()
-		        imgui.text_colored(monsters[ tostring(quest['monster5']) ]['name'], 0xff27f3f5)
-		        imgui.text('Target Num: ')
-		        imgui.same_line()
-		        imgui.text_colored(quest['_HuntTargetNum'], 0xff27f3f5)
-		        imgui.text('Time Limit: ')
-		        imgui.same_line()
-		        imgui.text_colored(quest['_TimeLimit'], 0xff27f3f5)
-		        imgui.text('Quest Life: ')
-		        imgui.same_line()
-		        imgui.text_colored(quest['_QuestLife'], 0xff27f3f5)
-		        imgui.text('Time of Day: ')
-		        imgui.same_line()
-		        imgui.text_colored(time_of_day_array[ quest['_StartTime'] +1], 0xff27f3f5)
-		        imgui.text('Hunter Num: ')
-		        imgui.same_line()
-		        imgui.text_colored(quest['_QuestOrderNum'], 0xff27f3f5)
-		    	imgui.text('Auth Status: ')
-		    	imgui.same_line()
-		    	imgui.text_colored((auth_status == 0 and "Pass" or auth_status_dict[auth_status]), (force_auth_pass and 0xff47ff59 or auth_status == 0 and 0xff47ff59 or 0xff1947ff))
-		        imgui.text('Quest Count: ')
-		        imgui.same_line()
-		        imgui.text_colored(quest_count, 0xff27f3f5)
-		        imgui.same_line()
-		        imgui.text('/  120')
-
-				map_c,map = imgui.combo('Map',map,maps_array)
-				imgui.new_line()
-
-				if tn_c and target_num == 3 then
-					monster_intruder_array = {'None'}
-				elseif tn_c and target_num < 3 then
-					monster_intruder_array = monster_intruder_array_
-					monster5 = index_of(monster_intruder_array,monsters[ tostring(quest['monster5']) ]['name'])
-				end
-
-				_,monster0 = imgui.combo('Monster 1',monster0,monster_main_array)
-				_,monster1 = imgui.combo('Monster 2',monster1,monster_extra_array)
-				_,monster2 = imgui.combo('Monster 3',monster2,monster_extra_array)
-				_,monster5 = imgui.combo('Intruder',monster5,monster_intruder_array)
-
-				imgui.new_line()
-				_,tod = imgui.combo('Time of Day',tod,time_of_day_array)
-				_,quest_lvl = imgui.slider_int('Quest Level', quest_lvl, 1, 200)
-				tn_c,target_num = imgui.slider_int('Target Num', target_num, 1, 3)
-				_,quest_life = imgui.slider_int('Quest Life', quest_life, 1, 9)
-				_,time_limit = imgui.slider_int('Time Limit', time_limit, 1, 50)
-				_,hunter_num = imgui.slider_int('Hunter Num', hunter_num, 1, 4)
-
-				monster0_id = monsters_id_table[ monster_main_array[ monster0 ] ]
-				monster1_id = monsters_id_table[ monster_extra_array[ monster1 ] ]
-				monster2_id = monsters_id_table[ monster_extra_array[ monster2 ] ]
-				monster5_id = monsters_id_table[ monster_intruder_array[ monster5 ] ]
-
-				if imgui.button('Edit Quest') then edit_quest(quest['data']) end
-				imgui.same_line()
-				if imgui.button('Generate Random Quest') then generate_random() end
-
-				if id_c then change_vals = true end
-
-				if id_c or auth_check then quest_check(quest['data']) end
-
-				if change_vals and dumped then reset_input() end
-			end
-		end
-		prev_game_state = game_state
-		imgui.tree_pop()
+re.on_frame(function()
+	if not reframework:is_drawing_ui() then
+	    is_opened = false
 	end
 end
 )
+
+re.on_draw_ui(function()
+    if imgui.button("Anomaly Investigations Editor "..version) then
+        is_opened = true
+    end
+
+    if is_opened then
+
+	    imgui.set_next_window_pos(window_pos, 1 << 3, window_pivot)
+	    imgui.set_next_window_size(window_size, 1 << 3)
+
+        if imgui.begin_window("Anomaly Investigations Editor "..version, is_opened, window_flags) then
+
+
+			if get_spacewatcher() then
+				game_state = get_spacewatcher():get_field('_GameState')
+			end
+
+			if qc_open or game_state ~= 4 or get_questman():call('isActiveQuest') then
+				imgui.text_colored('Mod works only in the lobby with quest counter closed.', 0xff1947ff)
+			else
+
+				if get_questman() and not dumped and game_state == 4
+				or game_state == 4 and prev_game_state ~= 4 then
+					reset_data()
+				end
+
+				if map_c then get_arrays() end
+
+				if filter_c then filter_names() end
+
+				_,force_auth_pass = imgui.checkbox('Force Authorization Pass', force_auth_pass)
+				filter_c,filter = imgui.input_text('Filter',filter)
+				id_c,id_pick = imgui.combo('Quest',id_pick,mystery_quests_names_filtered)
+		        quest_id = mystery_quests_names_filtered[ id_pick ]
+		        quest = mystery_quests[quest_id]
+
+		        if quest then
+			        imgui.text('Quest Level: ')
+			        imgui.same_line()
+			        imgui.text_colored(quest['_QuestLv'], 0xff27f3f5)
+			        imgui.text('Map: ')
+			        imgui.same_line()
+			        imgui.text_colored(maps_table[ quest['_MapNo'] ], 0xff27f3f5)
+			        imgui.text('Monster 1: ')
+					imgui.same_line()
+			        imgui.text_colored(monsters[ tostring(quest['monster0']) ]['name'], 0xff27f3f5)
+			        imgui.text('Monster 2: ')
+			        imgui.same_line()
+			        imgui.text_colored(monsters[ tostring(quest['monster1']) ]['name'], 0xff27f3f5)
+			        imgui.text('Monster 3: ')
+			        imgui.same_line()
+			        imgui.text_colored(monsters[ tostring(quest['monster2']) ]['name'], 0xff27f3f5)
+			        imgui.text('Intruder: ')
+			        imgui.same_line()
+			        imgui.text_colored(monsters[ tostring(quest['monster5']) ]['name'], 0xff27f3f5)
+			        imgui.text('Target Num: ')
+			        imgui.same_line()
+			        imgui.text_colored(quest['_HuntTargetNum'], 0xff27f3f5)
+			        imgui.text('Time Limit: ')
+			        imgui.same_line()
+			        imgui.text_colored(quest['_TimeLimit'], 0xff27f3f5)
+			        imgui.text('Quest Life: ')
+			        imgui.same_line()
+			        imgui.text_colored(quest['_QuestLife'], 0xff27f3f5)
+			        imgui.text('Time of Day: ')
+			        imgui.same_line()
+			        imgui.text_colored(time_of_day_array[ quest['_StartTime'] +1], 0xff27f3f5)
+			        imgui.text('Hunter Num: ')
+			        imgui.same_line()
+			        imgui.text_colored(quest['_QuestOrderNum'], 0xff27f3f5)
+			    	imgui.text('Auth Status: ')
+			    	imgui.same_line()
+			    	imgui.text_colored((auth_status == 0 and "Pass" or auth_status_dict[auth_status]), (force_auth_pass and 0xff47ff59 or auth_status == 0 and 0xff47ff59 or 0xff1947ff))
+			        imgui.text('Quest Count: ')
+			        imgui.same_line()
+			        imgui.text_colored(quest_count, 0xff27f3f5)
+			        imgui.same_line()
+			        imgui.text('/  120')
+
+					map_c,map = imgui.combo('Map',map,maps_array)
+					imgui.new_line()
+
+					if tn_c and target_num == 3 then
+						monster_intruder_array = {'None'}
+					elseif tn_c and target_num < 3 then
+						monster_intruder_array = monster_intruder_array_
+						monster5 = index_of(monster_intruder_array,monsters[ tostring(quest['monster5']) ]['name'])
+					end
+
+					_,monster0 = imgui.combo('Monster 1',monster0,monster_main_array)
+					_,monster1 = imgui.combo('Monster 2',monster1,monster_extra_array)
+					_,monster2 = imgui.combo('Monster 3',monster2,monster_extra_array)
+					_,monster5 = imgui.combo('Intruder',monster5,monster_intruder_array)
+
+					imgui.new_line()
+					_,tod = imgui.combo('Time of Day',tod,time_of_day_array)
+					_,quest_lvl = imgui.slider_int('Quest Level', quest_lvl, 1, 200)
+					tn_c,target_num = imgui.slider_int('Target Num', target_num, 1, 3)
+					_,quest_life = imgui.slider_int('Quest Life', quest_life, 1, 9)
+					_,time_limit = imgui.slider_int('Time Limit', time_limit, 1, 50)
+					_,hunter_num = imgui.slider_int('Hunter Num', hunter_num, 1, 4)
+
+					monster0_id = monsters_id_table[ monster_main_array[ monster0 ] ]
+					monster1_id = monsters_id_table[ monster_extra_array[ monster1 ] ]
+					monster2_id = monsters_id_table[ monster_extra_array[ monster2 ] ]
+					monster5_id = monsters_id_table[ monster_intruder_array[ monster5 ] ]
+
+					if imgui.button('Edit Quest') then edit_quest(quest['data']) end
+					imgui.new_line()
+
+					_,rand_id = imgui.combo('Random Quest Rank',rand_id,rand_rank_array)
+
+					if imgui.tree_node('Probabilities at 120 Research Level') then
+
+						if imgui.begin_table('',8, table_flags) then
+							for row=0,6 do
+
+								if (row % 2 == 0) then
+									imgui.table_next_row()
+								else
+									imgui.table_next_row(table_row_flags)
+								end
+
+								for col=0,7 do
+									imgui.table_set_column_index(col)
+									imgui.text(table_[row+1][col+1])
+								end
+
+							end
+							imgui.end_table()
+						end
+						imgui.tree_pop()
+					end
+
+					if imgui.button('Generate Random Quest') then generate_random(rand_rank[ rand_rank_array[rand_id] ]) end
+					imgui.same_line()
+					if imgui.button('Delete Quests') then wipe() end
+
+					if id_c then change_vals = true end
+
+					if id_c or auth_check then quest_check(quest['data']) end
+
+					if change_vals and dumped then reset_input() end
+				end
+			end
+			prev_game_state = game_state
+        	imgui.end_window()
+        else
+        	if is_opened then imgui.end_window() end
+        	is_opened = false
+        end
+    end
+end)
+
